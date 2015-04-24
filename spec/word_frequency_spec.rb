18c363742c06b2af05744623eed3df5e869d65cd
= <=>
2015-04-24 10:45:02 -0700

    Test that an empty test word returns 0.

--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -13,29 +13,64 @@
 #
 #   Output: 3
 
+require('rspec')
+require('word_frequency')
 
 # '*' indicates plain English spec description
 
-# * The word given is empty and so is the string; output is 0.
+describe('String#word_frequency') do
+  # * The word given is empty and so is the string; output is 0.
+  it('returns 0 when the test word is an empty string') do
+    expect(''.word_frequency()).to(eq(0))
+  end
+  
+  # # * The word given is empty but the string is not; output is 0.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is not empty but the string is; output is 0.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is exactly the same as the string; output is 1.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is not in the one-word string; output is 0.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is in the string once; output is 1.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is in the string multiple times; output is in agreement.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is a subset of a larger word in the string, such as
+  # #   'tac' is a subset of 'attack', appearing once; output is 0.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is a subset of a larger word in the string that has the
+  # #   same first first letters, such as 'at' is a subset of 'attack';
+  # #   output is 0.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
+  #
+  # # * The word given is a superset of another word, such as 'bat' is a superset
+  # #   of 'at'; output is 0.
+  # it('') do
+  #   expect(''.word_frequency()).to(eq())
+  # end
 
-# * The word given is empty but the string is not; output is 0.
-
-# * The word given is not empty but the string is; output is 0.
-
-# * The word given is exactly the same as the string; output is 1.
-
-# * The word given is not in the one-word string; output is 0.
-
-# * The word given is in the string once; output is 1.
-
-# * The word given is in the string multiple times; output is in agreement.
-
-# * The word given is a subset of a larger word in the string, such as
-#   'tac' is a subset of 'attack', appearing once; output is 0.
-
-# * The word given is a subset of a larger word in the string that has the
-#   same first first letters, such as 'at' is a subset of 'attack';
-#   output is 0.
-
-# * The word given is a superset of another word, such as 'bat' is a superset
-#   of 'at'; output is 0.
+end
