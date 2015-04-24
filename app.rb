3d61152db96d0964f762d0f586b6b148c6be4a75
Cory Spitzer <coryspitzer@gmail.com>
2015-04-24 15:41:19 -0700

    Process user input and display output.

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/word_frequency')
 
 get('/') do
   erb(:index)
+end
+
+get('/word_frequency') do
+  erb(:word_frequency)
+  word = params.fetch('word')
+  string = params.fetch('string')
+  @total_string = word.word_frequency(string).to_s()
 end
