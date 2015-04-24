a6ef1d48f1612bb67da0fe0ef192cb49d8e1dc90
= <=>
2015-04-24 11:37:13 -0700

    Write basic program logic to pass first four tests.

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,5 +1,12 @@
 class String
   define_method(:word_frequency) do |string=''|
-    0
+    words = string.split()
+    frequency = 0
+    words.each() do |word|
+      if self.==(word)
+        frequency += 1
+      end
+    end
+    frequency
   end
 end
