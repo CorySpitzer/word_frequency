22313b8de031d6457ecf962b6c5833e4eb1b8ff2
Cory Spitzer <coryspitzer@gmail.com>
2015-04-24 14:30:50 -0700

    Write basic spec.

---
+++ spec/word_frequency_integration_spec.rb
@@ -0,0 +1,17 @@
+require('./app')
+require('capybara/rspec')
+
+#What do these two lines do, *exactly?*
+Capybara.app = Sinatra::Application
+#set(:show_exceptions, false)
+
+describe('the word frequency path', :type => :feature) do
+  it('takes a word and a string from the user and returns the number of
+      occurances of that word in the string') do
+    visit('/')
+    fill_in('cat')
+    fill_in('The Cat cat in the Hat')
+    click_button('count')
+    expect(page).to(have_content(2))
+  end
+end
