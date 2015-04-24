a38f2a99b3c221430a9e707392dae59212dabf1f
= <=>
2015-04-24 12:19:30 -0700

    Add downcasing to both inputs to pass uppercase case specs.

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,9 +1,9 @@
 class String
   define_method(:word_frequency) do |string=''|
-    words = string.split()
+    words = string.downcase().split()
     frequency = 0
     words.each() do |word|
-      if self.==(word)
+      if self.downcase().==(word)
         frequency += 1
       end
     end
